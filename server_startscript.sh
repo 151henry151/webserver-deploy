79465dc6a4d38b05cd9f1e552b0a44a3aea535f8
Henry Romp <151henry151@gmail.com>
2023-08-19 04:07:05 -0400

    install phpmyadmin and mysql

--- server_startscript.sh
+++ server_startscript.sh
@@ -5,6 +5,7 @@
 adduser --disabled-password admin
 mkdir -p /home/admin/.ssh
 chmod 700 /home/admin/.ssh
+adduser admin sudo
 
 # Add t440p ssh key
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDHS6u7HVacvpB6bQtq0SS8/dpQPyH3S4d1/WTzBUWDNHrKM7HY0JDcUGiozo/lsNh83O/FxxeigZ3uyd6dCd2mtuii89WLQBToP24ofTmc2cm5qqZ7DNreCMyORhzc8IqvACXRBpOYhxrkKCDEoRXec0ukt6kwxNdYizs96VgS3HSaAPuIbqKMwITUOExHZ8RKrob/t+vhPeoOdmZxJyVzD/860irNuTvNmP/NkDnppw4hOGy1Mx+GjU1Y+oqlwmCIkAi6zMuwqCaDAfuYZgGFOtHSSMYLhSLCOAxwofBkZi8yV+Ltph2+gtDVDcAwsObwyfUOKos03uCvf7KpIclZzZDHIKwvcXiLB0U0UVIr/Z0OvW5o1tbXWmKhxn5uBGRBCJDnKaCVbpWoOfTYeWQM7N3Amxghzq3XkFpn6z6gYZNdkrTCygtb6w7p1ynQKYsouP3LyQqtMRgNSkdb1TBp77Ote4ww0vKYN6eKkSZNSl9awj8ldVJlTkrBVdE7Sts= henry@debian" > /home/admin/.ssh/authorized_keys
@@ -52,6 +53,18 @@ apt-get -y install mariadb-server mariadb-client
 mysql_secure_installation
 mysql -sfu root < "mysql_secure_installation.sql"
 
+# Download and install phpMyadmin
+wget -P /root/ https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-english.tar.gz
+mkdir /var/www/html/phpMyAdmin
+tar xvf phpMyAdmin-latest-english.tar.gz --strip-components=1 -C /var/www/html/phpMyAdmin
+cp /var/www/html/phpMyAdmin/config.sample.inc.php /var/www/html/phpMyAdmin/config.inc.php
+sed -i 's/'blowfish_secret'] = '';/'blowfish_secret'] = 'ceonoi4$$allamwow5969';/g'
+chmod 660 /var/www/html/phpMyAdmin/config.inc.php
+chown -R www-data:www-data /var/www/html/phpMyAdmin
+systemctl restart apache2
+
 
 # Restart ssh daemon
 systemctl restart sshd.service
+
+
